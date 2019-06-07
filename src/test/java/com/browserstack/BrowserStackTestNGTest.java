e292f8719e21c58d72ff648fdd4825b7a62fe896
Swapnil Agarwal <swapnil.a@browserstack.com>
2019-06-07 12:36:37 +0530

    commenting the code

--- src/test/java/com/browserstack/BrowserStackTestNGTest.java
+++ src/test/java/com/browserstack/BrowserStackTestNGTest.java
@@ -18,22 +18,25 @@ public class BrowserStackTestNGTest {
     public WebDriver driver;
 
     @BeforeMethod(alwaysRun = true)
-    @org.testng.annotations.Parameters(value = { "config", "environment" })
+    @org.testng.annotations.Parameters(value = { "config", "environment" }) // config and environment are set in suite.testng.xml
     @SuppressWarnings("unchecked")
     public void setUp(String config_file, String environment) throws Exception {
         JSONParser parser = new JSONParser();
-        JSONObject config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
+        JSONObject config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file)); // read config file
         JSONObject envs = (JSONObject) config.get("environments");
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+        DesiredCapabilities capabilities = new DesiredCapabilities(); 
 
         Map<String, String> envCapabilities = (Map<String, String>) envs.get(environment);
         Iterator it = envCapabilities.entrySet().iterator();
+
+        // set capabilities for the environment, set in suite.conf.json file
         while (it.hasNext()) {
             Map.Entry pair = (Map.Entry) it.next();
             capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
         }
 
+        // set capabilities common for all the environment, set in suite.conf.json file
         Map<String, String> commonCapabilities = (Map<String, String>) config.get("capabilities");
         it = commonCapabilities.entrySet().iterator();
         while (it.hasNext()) {
@@ -43,22 +46,26 @@ public class BrowserStackTestNGTest {
             }
         }
 
+        // set BrowserStack username
         String username = System.getenv("BROWSERSTACK_USERNAME");
         if (username == null) {
             username = (String) config.get("user");
         }
 
+        // set BrowserStack access key
         String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
         if (accessKey == null) {
             accessKey = (String) config.get("key");
         }
 
+        // initialize a remote web driver instance
         driver = new RemoteWebDriver(
                 new URL("http://" + username + ":" + accessKey + "@" + config.get("server") + "/wd/hub"), capabilities);
     }
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() throws Exception {
+        // call driver.quit
         driver.quit();
     }
 }
